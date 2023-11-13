1e90bc700825879854421f4bfa2a38622182e927
ugauniyal <ugauniyal@outlook.com>
2023-11-13 14:50:04 +0530

    added interest feature in settings

--- flutter projects/workout_app/lib/SettingPage.dart
+++ flutter projects/workout_app/lib/SettingPage.dart
@@ -8,6 +8,8 @@ class SettingsOnePage extends StatefulWidget {
 }
 
 class _SettingsOnePageState extends State<SettingsOnePage> {
+  List<String> interests = [];
+
   bool enableDiscovery = true;
   bool sendReadReceipt = true;
   bool enableDarkMode = true;
@@ -29,6 +31,80 @@ class _SettingsOnePageState extends State<SettingsOnePage> {
   bool menSelected = false;
   bool womenSelected = false;
   bool bothSelected = false;
+
+  bool isInterestExpanded = false;
+
+  Widget _buildInterestsList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: interests.length,
+      itemBuilder: (context, index) {
+        return InterestItem(title: interests[index]);
+      },
+    );
+  }
+
+  Widget _buildAddInterestButton() {
+    return ListTile(
+      title: ElevatedButton(
+        onPressed: () {
+          _showAddInterestDialog(context);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green, // Change button color
+          foregroundColor: Colors.white, // Change text color
+        ),
+        child: Text("Add Interest"),
+      ),
+    );
+  }
+
+  void _showAddInterestDialog(BuildContext context) {
+    TextEditingController _interestController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Add Interest", style: TextStyle(color: Colors.black)),
+          content: TextField(
+            controller: _interestController,
+            decoration: InputDecoration(labelText: 'Enter interest'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel', style: TextStyle(color: Colors.black)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String enteredInterest = _interestController.text.trim();
+                if (enteredInterest.isNotEmpty) {
+                  // Do something with the entered interest
+                  interests.add(enteredInterest);
+                  setState(() {}); // Assuming you are using StatefulWidget
+                  Navigator.pop(context); // Close the dialog
+                } else {
+                  // Show Snackbar if the user didn't enter anything
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please enter an interest'),
+                    ),
+                  );
+                }
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget _buildDistanceSlider(BuildContext context) {
@@ -245,7 +321,7 @@ class _SettingsOnePageState extends State<SettingsOnePage> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               CheckboxListTile(
-                                title: Text("Women"),
+                                title: Text("Men"),
                                 value: menSelected,
                                 activeColor: Colors.black,
                                 onChanged: (bool? value) {
@@ -435,10 +511,17 @@ class _SettingsOnePageState extends State<SettingsOnePage> {
                       color: Colors.black,
                     ),
                     title: Text("Interests"),
-                    trailing: Icon(Icons.keyboard_arrow_right),
+                    trailing: isInterestExpanded
+                        ? Icon(Icons.keyboard_arrow_down, color: Colors.black)
+                        : Icon(Icons.keyboard_arrow_right, color: Colors.black),
+                    onExpansionChanged: (expanded) {
+                      setState(() {
+                        isInterestExpanded = expanded;
+                      });
+                    },
                     children: <Widget>[
-                      // Additional content for "Interests"
-                      // You can add more widgets as needed
+                      _buildInterestsList(),
+                      _buildAddInterestButton(),
                     ],
                   )
                 ],
@@ -508,35 +591,51 @@ class _SettingsOnePageState extends State<SettingsOnePage> {
             const SizedBox(
               height: 5.0,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Text(
-                "Dark Mode",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.indigo,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: SwitchListTile(
-                  dense: true,
-                  activeColor: Colors.black,
-                  contentPadding: const EdgeInsets.all(0),
-                  value: enableDarkMode,
-                  title:
-                      Text('Enable Dark Mode', style: TextStyle(fontSize: 13)),
-                  onChanged: (val) {
-                    setState(() {
-                      enableDarkMode = val;
-                    });
-                  }),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 8.0),
+            //   child: Text(
+            //     "Dark Mode",
+            //     style: TextStyle(
+            //       fontSize: 15.0,
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.indigo,
+            //     ),
+            //   ),
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 8.0),
+            //   child: SwitchListTile(
+            //       dense: true,
+            //       activeColor: Colors.black,
+            //       contentPadding: const EdgeInsets.all(0),
+            //       value: enableDarkMode,
+            //       title:
+            //           Text('Enable Dark Mode', style: TextStyle(fontSize: 13)),
+            //       onChanged: (val) {
+            //         setState(() {
+            //           enableDarkMode = val;
+            //           // Provider.of<ThemeProvider>(context, listen: false)
+            //           //     .toggleTheme();
+            //         });
+            //       }),
+            // ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class InterestItem extends StatelessWidget {
+  final String title;
+
+  InterestItem({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      // Customize ListTile as needed
     );
   }
 }
